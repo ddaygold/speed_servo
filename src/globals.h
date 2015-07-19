303a2893fb70cbea0322dbc674814993d32c69ea
Peter Chinetti <peter@chinetti.me>
2015-07-19 15:29:32 -0400

    refactored to help the next person to look at this

--- src/globals.h
+++ src/globals.h
@@ -2,29 +2,40 @@
 #include <Servo.h>
 #include <PID_v1.h>
 #include <EEPROM.h>
+//Pins for the Quad encoder
 #define PINA 2
 #define PINB 3
+//Pin to specify the i2c address
+//High is address 1, low is address 0
 #define ADDRESS_PIN 13
 volatile double speed = 0;
-#define SPEED 0
 volatile double pterm = 0.0;
-#define PTERM 1
 volatile double iterm = 0.0;
-#define ITERM 2
 volatile double dterm = 0.0;
-#define DTERM 3
+enum command{
+	SPEED   = 0,
+	PTERM   = 1,
+	ITERM   = 2,
+	DTERM   = 3,
+	VOLTAGE = 4,
+	ESTOP   = 5
+};
 #define VOLTAGE_PIN A0
-#define VOLTAGE 4
-#define ESTOP 5
 
 #define VICTOR_PIN 5
 Servo victor;
 
 volatile double ticks = 0;
 int last_ticks = 0;
-int current_speed = 1510;
 
-double controller_output = 0;
+  /*
+    If the Victor motor controller PWM width extremes are 870 microsecond and 2140 microseconds, with
+    a center at 1510 microseconds, we have a width of 635 microseconds
+  */
+#define PWM_CENTER 1510
+#define PWM_WIDTH  635
+int current_speed = 0;
+double controller_output = PWM_CENTER;
 PID controller(&ticks, &controller_output, &speed,
 	       pterm,iterm,dterm,
 	       DIRECT);
@@ -36,3 +47,4 @@ void writeTicks();
 void setParam(int incoming, volatile double * dest);
 void setParam(int incoming, volatile int * dest);
 void spin();
+void printDebug();
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,64 +1,62 @@
 #include "globals.h"
 
 void setup(){
+  //Load saved tunings from EEPROM, initalize the controller
   pterm = load_controller_tuning(PTERM);
   iterm = load_controller_tuning(ITERM);
   dterm = load_controller_tuning(DTERM);
-  //We need to update the controler after
-  //loading from EEPROM
   controller.SetTunings(pterm,iterm,dterm);
+  controller.SetMode(AUTOMATIC);
+  controller.SetOutputLimits(-1*PWM_WIDTH,PWM_WIDTH);
+  controller.SetSampleTime(100);
+
   Serial.begin(9600);
+  //A and B channels of the Quad encoder
   pinMode(PINA, INPUT);
   pinMode(PINB, INPUT);
-  pinMode(ADDRESS_PIN, INPUT);
-  pinMode(12,OUTPUT);
-  digitalWrite(12,HIGH); //so we can jumper over to pin 12
+  attachInterrupt(0,spin,RISING);
+
   pinMode(VICTOR_PIN, OUTPUT);
   victor.attach(VICTOR_PIN);
-  victor.writeMicroseconds(1510);
-  attachInterrupt(0,spin,RISING);
+  victor.writeMicroseconds(PWM_CENTER); 
+
+  /*
+     The Arduino has a ground pin, then pin 13, then pin 12
+     We set 13 as the input, and set 12 high so we can jumper to 
+     either GND or 12 (HIGH) to set the i2c address 
+   */
+  pinMode(ADDRESS_PIN, INPUT);
+  pinMode(12,OUTPUT);
+  digitalWrite(12,HIGH); 
   int address = (digitalRead(ADDRESS_PIN) == HIGH)?2:1;
   Serial.print("Address: ");
   Serial.println(address);
   Wire.begin(address);
-  Wire.onReceive(receive);
-  Wire.onRequest(writeTicks);
-  controller.SetMode(AUTOMATIC);
-  controller.SetOutputLimits(-635,635);
-  controller.SetSampleTime(100);
+  Wire.onReceive(receive);//This requires a protocol to understand which parameter is being changed
+  Wire.onRequest(writeTicks);//This is always the speed
 }
 
 void loop(){
   /*
-    If the extremes are 870 microsecond and 2140 microseconds, with
-    a center at 1510 microseconds, we have a width of 635 microseconds
-  */
+     We only set the external tuning variables in the i2c
+     receive routine. We load the controller with new values periodically
+     this assumes that you are setting the values infrequently, so the lag period 
+     doesn't have a large impact.
+   */
   if(millis() - last_tuning_update > 1000){
     controller.SetTunings(pterm,iterm,dterm);
     last_tuning_update = millis();
   }
   if(controller.Compute() || controller.GetMode() == MANUAL){
     if(controller.GetMode() != MANUAL){
-      int output = (int)controller_output + 1510;
+      int output = (int)controller_output + PWM_CENTER;
       victor.writeMicroseconds(output);
     } else {
-      victor.writeMicroseconds(1510);
+      //The controller is only in MANUAL when it has been estopped
+      //Therefore, write the Victor's zero value
+      victor.writeMicroseconds(PWM_CENTER);
     }
-    Serial.print("Speed: ");
-    Serial.print(speed);
-    Serial.print(" ticks: ");
-    Serial.print(ticks);
-    Serial.print(" p: ");
-    Serial.print(pterm);
-    Serial.print(" i: ");
-    Serial.print(iterm);
-    Serial.print(" d: ");
-    Serial.print(dterm);
-    Serial.print(" OUTPUT: ");
-    Serial.println(controller_output);
-    if(controller.GetMode() == MANUAL){
-      Serial.println("****ESTOP****");
-    }
+    printDebug();
     last_ticks = ticks;
     ticks = 0;
   }
@@ -66,7 +64,7 @@ void loop(){
 
 
 void receive(int incoming){
-  switch(Wire.read()){
+  switch((enum command)Wire.read()){
   case SPEED:
     if(controller.GetMode() == MANUAL){
       controller.SetMode(AUTOMATIC);
@@ -94,6 +92,26 @@ void receive(int incoming){
 
 void writeTicks(){
   Wire.write((char *) &last_ticks, sizeof(int));
+}
+
+void printDebug(){
+    //All serial printing is for debugging only.
+    //Control information happens over i2c
+    Serial.print("Speed: ");
+    Serial.print(speed);
+    Serial.print(" ticks: ");
+    Serial.print(ticks);
+    Serial.print(" p: ");
+    Serial.print(pterm);
+    Serial.print(" i: ");
+    Serial.print(iterm);
+    Serial.print(" d: ");
+    Serial.print(dterm);
+    Serial.print(" OUTPUT: ");
+    Serial.println(controller_output);
+    if(controller.GetMode() == MANUAL){
+      Serial.println("****ESTOP****");
+    }
 }
 void setParam(int incoming, volatile int * dest){
   if(incoming != sizeof(int) +1){
